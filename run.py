e736b5e545539c8f0ba6fbcc2c1ee1569cba8bf4
Tadas Talaikis <talaikis.tadas@gmail.com>
2020-03-12 15:57:33 +0200

    random data

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
 from os.path import join, dirname, abspath
+import random
+import time
 
 from seaborn import heatmap
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from pandas import read_json
+from pandas import read_json, DataFrame, to_datetime
 import seaborn as sns
 
 BASE_DATA = join(dirname(abspath(__file__)))
 fig_size = (9, 6)
 
+def rnd_timestamp(start, end):
+    return random.random() * (end - start) + start
+
 def cumulate_returns(x):
     return x.cumsum()[-1]
+
+def rndData():
+    idx = []
+    ret = []
+    for _ in range(0, 1000):
+        dt = rnd_timestamp(1445126400000, 1583280000000)
+        val = random.random()
+        idx.append(dt)
+        ret.append(val)
+    return DataFrame(ret, index=to_datetime(idx,  unit='ms'))
 
 def plot_monthly_heatmap(returns, name):
     try:
@@ -32,5 +47,7 @@ def plot_monthly_heatmap(returns, name):
         print('plot_monthly_heatmap', e)
 
 if __name__ == '__main__':
-    df = read_json(join(BASE_DATA, 'ret.json'), orient='index')
+    # df = read_json(join(BASE_DATA, 'ret.json'), orient='index')
+    df = rndData()
+    df.columns = ['returns']
     plot_monthly_heatmap(df['returns'], 'test')
